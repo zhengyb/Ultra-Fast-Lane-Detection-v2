ea9aac0e8794c07a3eb73b3e62f7e317d8fbd4d7
zhengyb <zhengyb@inhand.com.cn>
2025-04-03 12:08:05 -0400

    Donot depend on pytorch while speed testing

--- deploy/trt_infer_speed_test.py
+++ deploy/trt_infer_speed_test.py
@@ -1,8 +1,74 @@
 import cv2
 import time
 import argparse
-from trt_infer import UFLDv2  # 复用已有的推理类
 import numpy as np
+import tensorrt as trt
+import pycuda.driver as cuda
+import pycuda.autoinit
+import argparse
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+from utils.config import Config
+
+
+
+class UFLDv2:
+    def __init__(self, engine_path, config_path, ori_size):
+        self.logger = trt.Logger(trt.Logger.ERROR)
+        with open(engine_path, "rb") as f, trt.Runtime(self.logger) as runtime:
+            self.engine = runtime.deserialize_cuda_engine(f.read())
+        self.context = self.engine.create_execution_context()
+
+        self.inputs = []
+        self.outputs = []
+        self.allocations = []
+        for i in range(self.engine.num_bindings):
+            is_input = False
+            if self.engine.binding_is_input(i):
+                is_input = True
+            name = self.engine.get_binding_name(i)
+            dtype = self.engine.get_binding_dtype(i)
+            shape = self.engine.get_binding_shape(i)
+            if is_input:
+                self.batch_size = shape[0]
+            size = np.dtype(trt.nptype(dtype)).itemsize
+            for s in shape:
+                size *= s
+            allocation = cuda.mem_alloc(size)
+            binding = {
+                'index': i,
+                'name': name,
+                'dtype': np.dtype(trt.nptype(dtype)),
+                'shape': list(shape),
+                'allocation': allocation,
+            }
+            self.allocations.append(allocation)
+            if self.engine.binding_is_input(i):
+                self.inputs.append(binding)
+            else:
+                self.outputs.append(binding)
+
+        cfg = Config.fromfile(config_path)
+        self.ori_img_w, self.ori_img_h = ori_size
+        self.cut_height = int(cfg.train_height * (1 - cfg.crop_ratio))
+        self.input_width = cfg.train_width
+        self.input_height = cfg.train_height
+        self.num_row = cfg.num_row
+        self.num_col = cfg.num_col
+        self.row_anchor = np.linspace(0.42, 1, self.num_row)
+        self.col_anchor = np.linspace(0, 1, self.num_col)
+
+    def forward_core(self, img):
+        cuda.memcpy_htod(self.inputs[0]['allocation'], img)
+        self.context.execute_v2(self.allocations)
+        preds = {}
+        for out in self.outputs:
+            output = np.zeros(out['shape'], out['dtype'])
+            cuda.memcpy_dtoh(output, out['allocation'])
+            preds[out['name']] = output
+        return preds
+
 
 
 def test_fps(engine_path, config_path, ori_size, test_seconds=30):
